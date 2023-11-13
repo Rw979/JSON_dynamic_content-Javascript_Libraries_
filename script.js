571cbc5f17a28aa14ca6bdddcebb2676fada404f
RuxiWang <rw2893@nyu.edu>
2023-11-12 21:05:00 -0500

    saving

--- script.js
+++ script.js
@@ -66,6 +66,78 @@ let twiceCollection = [
     "id": "tzuyu",
     "description": "Birth: 1999.06.14",
     "image": "https://Rw979.github.io/JSON_dynamic_content-Javascript_Libraries_/Profile_Tzuyu.jpeg"
+  },
+  {
+    "itemTitle": "ZONE",
+    "category": "Album",
+    "id": "zone",
+    "secondTitle":"TRACK LIST",
+    "description": ["1. Killin’ Me Good *TITLE", "2. Talkin’ About It (Feat. 24kGoldn)", "3. Closer", "4. Wishing On You", "5. Don’t Wanna Go Back (Duet with 헤이즈)", "6. Room", "7. Nightmare", "Release Date: 2023-08-18"],
+    "image": "https://Rw979.github.io/JSON_dynamic_content-Javascript_Libraries_/Album_zone.jpeg"
+  },
+  {
+    "itemTitle": "READY TO BE",
+    "category": "Album",
+    "id": "readytobe",
+    "secondTitle": "TRACK LIST",
+    "description": ["1. SET ME FREE *Title","2. MOONLIGHT SUNRISE","3. GOT THE THRILLS","4. BLAME IT ON ME","5. WALLFLOWER","6. CRAZY STUPID LOVE","7. SET ME FREE (ENG)","Release Date: 2023-03-10"],
+    "image": "https://Rw979.github.io/JSON_dynamic_content-Javascript_Libraries_/Album_Ready_To_Be.jpeg"
+  },
+  {
+    "itemTitle": "MOONLIGHT SUNRISE",
+    "category": "Album",
+    "id": "moonlighhtsunrise",
+    "secondTitle": "TRACK LIST",
+    "description": ["1. MOONLIGHT SUNRISE *TITLE","Release Date: 2023-01-20"],
+    "image": "https://Rw979.github.io/JSON_dynamic_content-Javascript_Libraries_/Album_Moonlight_Sunrise.jpeg"
+  },
+  {
+    "itemTitle": "BETWEEN 1&2",
+    "category": "Album",
+    "id": "between1&2",
+    "secondTitle": "TRACK LIST",
+    "description": ["1. Talk that Talk *Title","2. Queen of Hearts","3. Basics","4. Trouble","5. Brave","6. Gone","7. When We Were Kids","Release Date: 2022-08-26"],
+    "image": "https://Rw979.github.io/JSON_dynamic_content-Javascript_Libraries_/Album_Between%201%262.jpeg"
+  },
+  {
+    "itemTitle": "IM NAYEON",
+    "category": "Album",
+    "id": "imnayeon",
+    "secondTitle": "TRACK LIST",
+    "description": ["1. POP!","2. NO PROBELM (Feat. Felix of Stray Kids)","3. LOVE COUNTDOWN (Feat. Wonstein)","4. CANDYFLOSS","5. ALL OR NOTHING","6. HAPPY BIRTHDAY TO YOU","7. 노을만 예쁘다 (SUNSET)","Release Date: 2022-06-24"],
+    "image": "https://Rw979.github.io/JSON_dynamic_content-Javascript_Libraries_/"
+  },
+  {
+    "itemTitle": "Formula of Love: O+T=<3",
+    "category": "Album",
+    "id": "formulaoflove",
+    "secondTitle": "TRACK LIST",
+    "description": ["1. SCIENTIST *Title","2. MOONLIGHT","3. ICON","4. CRUEL","5. REAL YOU","6. F.I.L.A","7. LAST WALTZ","8. ESPRESSO","9. 알고 싶지 않아 (REWIND)","10. 선인장 (CACTUS)","11. PUSH & PULL (JIHYO, SANA, DAHYUN)","12. HELLO (NAYEON, MOMO, CHAEYOUNG)","13. 1, 3, 2 (JEONGYEON, MINA, TZUYU)","14. CANDY","15 The Feels (Korean Ver.)","16. The Feels","17. SCIENTIST (R3HAB Remix)","Release Date: 2021-11-12"],
+    "image": "https://Rw979.github.io/JSON_dynamic_content-Javascript_Libraries_/Album_Formula_of_Love.png"
+  },
+  {
+    "itemTitle": "CRY FOR ME",
+    "category": "Album",
+    "id": "cryforme",
+    "secondTitle": "TRACK LIST",
+    "description": ["01. CRY FOR ME","Release Date: 2020-12-18"],
+    "image": "https://Rw979.github.io/JSON_dynamic_content-Javascript_Libraries_/Album_Cry_For_Me.jpeg"
+  },
+  {
+    "itemTitle": "Eyes wide open",
+    "category": "Album",
+    "id": "eyewideopen",
+    "secondTitle": "TRACK LIST",
+    "description": ["01. I CAN'T STOP ME","02. HELL IN HEAVEN","03. UP NO MORE","04. DO WHAT WE LIKE","05. BRING IT BACK","06. BELIEVER","07. QUEEN","08. GO HARD","09. SHOT CLOCK","10. HANDLE IT","11. DEPEND ON YOU","12. SAY SOMETHING","13. BEHIND THE MASK","Release Date: 2020-10-26"],
+    "image": "https://Rw979.github.io/JSON_dynamic_content-Javascript_Libraries_/Album_Eyes_wide_Open.png"
+  },
+  {
+    "itemTitle": "Feel Special",
+    "category": "Album",
+    "id": "feelspecial",
+    "secondTitle": "TRACK LIST",
+    "description": ["01. FEEL SPECIAL","02. RAINBOW","03. GET LOUD","04. TRICK IT","05. LOVE FOOLISH","06. 21:29","07. BREAKTHROUGH (Korean Ver.)","Release Date: 2019-09-23"],
+    "image": "https://Rw979.github.io/JSON_dynamic_content-Javascript_Libraries_/Album_Feel_Special.jpeg"
   }
 ];
 
@@ -121,7 +193,7 @@ document.addEventListener("DOMContentLoaded", function () {
   /*Json for mainContainer_right*/
   let submenutext = {
     "Profile": ["NAYEON", "JEONGYEON", "MOMO", "SANA", "JIHYO", "MINA", "DAHYUN", "CHAEYOUNG", "TZUYU"],
-    "Album": ["Ready to Love", "Let Me Know"]
+    "Album": ["ZONE", "READY TO BE","MOONLIGHT SUNRISE", "BETWEEN 1&2","IM NAYEON", "Formula of Love: O+T=<3","CRY FOR ME", "Eyes wide open","Feel Special"]
   };
 
   for (let section in submenutext) {
@@ -134,7 +206,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
       let newLink = document.createElement("a");
       newLink.innerText = submenutext[section][i];
-      newLink.href = "index.html?section=Profile&id=" + twiceCollection[i].id;
+      newLink.href = "index.html?section=" + section + "&id=" + twiceCollection[i].id;
       mainContainerRightElement.appendChild(newLink);
 
     }
@@ -144,7 +216,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function createTwicePreview(incomingJSON){
 	let newPreviewLink = document.createElement("A");
-	newPreviewLink.href = "index.html?section=profile&id=" + incomingJSON["id"];
+	newPreviewLink.href = "index.html?section=" + incomingJSON["category"].toLowerCase() + "&id=" + incomingJSON["id"];
 
 	let newPreviewElement = document.createElement("DIV");
 	newPreviewLink.appendChild(newPreviewElement);
@@ -169,7 +241,6 @@ function createTwicePage(incomingJSON) {
     let newTitle = document.createElement('h2');
     newTitle.innerText = incomingJSON.itemTitle;
 
-    // 修正了属性名称从 'descirption' 到 'description'
     let newDescription = document.createElement('p');
     newDescription.innerText = incomingJSON.description;
 
@@ -177,14 +248,27 @@ function createTwicePage(incomingJSON) {
     newImage.src = incomingJSON.image;
     newImage.alt = incomingJSON.itemTitle;
 
-    newDiv.appendChild(newTitle);
-    newDiv.appendChild(newDescription);
-    newDiv.appendChild(newImage);
+    if (incomingJSON.category === "Profile") {
+        newDiv.appendChild(newTitle);
+        newDiv.appendChild(newDescription);
+        newDiv.appendChild(newImage);
+    } else if (incomingJSON.category === "Album") {
+        newDiv.appendChild(newImage);
+        newDiv.appendChild(newTitle);
+
+        if (incomingJSON.secondTitle) {
+            let newTitle2 = document.createElement('h3');
+            newTitle2.innerText = incomingJSON.secondTitle;
+            newDiv.appendChild(newTitle2);
+        }
+
+        newDiv.appendChild(newDescription);
+    } else if (incomingJSON.category === "Gallery") {
+        newDiv.appendChild(newImage);
+        newDiv.appendChild(newTitle);
+        newDiv.appendChild(newDescription);
+    }
 
     mainContainerLeftElement.appendChild(newDiv);
 }
-
-
- 
-
 
